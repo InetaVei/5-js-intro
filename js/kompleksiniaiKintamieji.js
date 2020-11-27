285165c7a441bd195046607c07b2467d401faaf9
InetaVei <74532995+InetaVei@users.noreply.github.com>
2020-11-27 11:01:23 +0200

    Primityvios vs kompleksines reiksmes

--- js/kompleksiniaiKintamieji.js
+++ js/kompleksiniaiKintamieji.js
@@ -1,4 +1,7 @@
-const a = 5;
+/* PRIMITYVIOS REIKSMES */
+
+
+let a = 5;
 let b = 100;
 
 console.log(a);
@@ -8,3 +11,25 @@ b = a;
 
 console.log(a);
 console.log(b);
+
+a = 88; 
+
+/* KOMPLEKSINES REIKSMES
+net jei ir yra const, saraso vidaus dalyvius galiu koreguoti, nurodant atskirai kad dalyvis is saraso, kurio nr yra toks ir toks, nuo siol bus toks taciau pacio saraso - ne.Jei noriu keisti visa pati sarasa - tuomet pakeiciu iniciala i let.
+
+Jei daromi kazkokie pakeitimai, listu koregavimai ir tame dalyvauja abu listai, tai jie abu mato ta pacia ATNAUJINTA informacija. Priesingei nei prisimyviose reiksmese, kompleksinese reiksmese kintamieji pradeda dalintis informacija, atmintimi*/
+
+let listA = [11,22];
+let listB = [800, 900];
+
+listA[0] = 2; 
+listB = listA;
+
+console.log(listA);
+console.log(listB);
+
+listA = [7, 77, 777];
+
+console.log(listA);
+console.log(listB);
+
--- js/veiksmaiArray.js
+++ js/veiksmaiArray.js
@@ -1,4 +1,25 @@
-const pazymiai = [10, 2, 7];
+/* array - rinkiniai, matrica */
+
+const pazymiai = [10, 2, 7, 6];
 const zodynas = ['labas', 'rytas', 'Lietuva'];
 
-// pazymiai ->
+
+/* array (siuo atveju --> pazymiai) ir kagi galima su jais nuveikti:
+const vidurkis = (10 + 2 + 7 + 6) / 4;
+ - vidurki isvesti;
+ - isrikiuoti (min, max);
+ - surasti ar turi konkretu skaiciu;
+ - surasti min, max reiksme; 
+ */
+
+ // skaiciuojame vidurki
+ const pazymiai = [10, 2, 7, 6];
+ console.log ('Vidurkis', vidurkis);
+ /* arba, jei pvz sarase yra labai daug skaiciu, tai tuomet naudojam funkcija .length ji suskaiciuoja KIEK sarase yra dalyviu, o pacius saraso dalyvius geriausia israryti asmeniskai pagal ju eiliskuma, pradedant nuo 0, taip apsisaugosim nuo nefunkcionalumo jei kartais pasikeistu kazkuris saraso dalyvis  */
+
+ const sarasoSkaiciuSuma = pazymiai[0] + pazymiai[1] + pazymiai[2] + pazymiai[3];
+ const skaiciuKiekis = pazymiai.length;
+ const vidurkis = sarasoSkaiciuSuma / skaiciuKiekis;
+ console.log ('Vidurkis', vidurkis);
+
+
