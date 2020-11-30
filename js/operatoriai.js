8b0933fd3b660164052f49c2043fac890f9acd60
InetaVei <74532995+InetaVei@users.noreply.github.com>
2020-11-30 11:57:51 +0200

    operatoriai

---
+++ js/operatoriai.js
@@ -0,0 +1,30 @@
+/* 
+
+Matematikos operatoriai:
+
+2+2
+2*8
+4-7
+16/2
+
+*/
+
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = pinigine + 100;
+console.log(pinigine);
+
+
+/* 
+++ padidinti vienu vienetu
+-- sumazinti vienu vienetu
+*/
+
+let i = 0;
+console.log(i);
+
+i = i + 1;
+i += 1;
+i++; 
+console.log(i);
