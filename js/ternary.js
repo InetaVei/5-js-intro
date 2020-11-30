70c6a77122dfe3337ab669ea10cc057d21fa1b90
InetaVei <74532995+InetaVei@users.noreply.github.com>
2020-11-30 11:15:01 +0200

    ternary

---
+++ js/ternary.js
@@ -0,0 +1,37 @@
+/*
+Ternary operator
+
+liaudiskai: inline if'as, kuris savyje turi r gali tureti TIK true ir false dalis.
+
+if'o klausimas? Jei teigiamas ats: jeigu neigiamas ats
+*/
+
+console.log(4 > 2 ? 'Daugiau' : 'Ne daugiau');
+
+const amziausRiba = 40;
+
+const amzius = 99;
+
+const verdiktas = amzius >= amziausRiba ? 'I Prezitentus' : 'dar palauk';
+
+console.log(`Jei tau ${amzius} - ${verdiktas}`);
+
+
+console.log('------------------');
+
+const sezonas = 'vasara';
+const temp = 10;
+
+if (sezonas === 'vasara');
+   if (temp > 10) {
+       console.log('Maike');
+   } else {
+       console.log('golfas');
+   }
+} else {
+       if (temp >10 {
+           console.log('megztinis');
+       } else {
+           console.log('striuke');
+       }
+   }
