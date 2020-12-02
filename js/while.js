97ff2ba547b940a57c76314343ccc0abff9b8831
InetaVei <74532995+InetaVei@users.noreply.github.com>
2020-12-02 11:54:33 +0200

    while

---
+++ js/while.js
@@ -0,0 +1,20 @@
+/* 
+while() {}
+    proceduros atliekamos tol-kol tenkinama kazkokia salyga (-os);*/
+ 
+    let i = 0;
+     
+    while (i < 5) {
+        console.log('labas');
+        i++;
+    }
+
+    /* 
+    sumuoti atsitiktini skaiciu tol kol gausim suma 10 */
+
+    let randomSum = 0;
+
+    while(randomSum < 10) {
+        console.log
+
+    }
